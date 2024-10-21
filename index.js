35b87fa08ebfc6e13dedc8f493d74651ebafd384
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-10-21 16:24:52 +0200

    add comment to line 9 and add P next to all comments made by me

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  */
 
 // Given Parameters
+//P: renamed variable names
 const initialVelocityKmh = 10000; // velocity (km/h)
 const accelerationMs2 = 3; // acceleration (m/s^2)
 const timeSeconds = 3600; // seconds (1 hour)
@@ -13,28 +14,28 @@ const initialDistanceKm = 0; // distance (km)
 const initialFuel = 5000; // remaining fuel (kg)
 const fuelBurnRateKgPerSecond = 0.5; // fuel burn rate (kg/s)
 
-//function to convert acceleration from m/s^2 to km/h^2
+//P: function to convert acceleration from m/s^2 to km/h^2
 const convertAccelartaionToKmh2 = (accelerationMs2) => {
   return accelerationMs2 * 12960; // 3600^2 * 0.001(meters to km- 1km = 0.001m)
 };
 
-//convert accelaration from m/s^2 to km/h^2
+//P: convert accelaration from m/s^2 to km/h^2
 const accelerationKmh2 = convertAccelartaionToKmh2(accelerationMs2);
 
-// convert time from seconds to hours
+//P: convert time from seconds to hours
 const timeHours = timeSeconds / 3600;
 
-//function to calculate the new velocity 
+//P: function to calculate the new velocity 
 const calcNewVel = (initialVelocityKmh, accelerationKmh2, timeHours) => {
   return initialVelocityKmh + accelerationKmh2 * timeHours;
   
 };
 
-//calculate new values
+//P: calculate new values
 const newDistance = initialDistanceKm + (initialVelocityKmh * timeHours); // to calculate new distance
 const remainingFuelKg = initialFuel - (fuelBurnRateKgPerSecond * timeSeconds); //to calculate remailing fuel 
 
-//calculate new velocity, catch potential errors
+//P: calculate new velocity, catch potential errors
 let newVelocityKmh;
 try {
   newVelocityKmh = calcNewVel(initialVelocityKmh, accelerationKmh2, timeHours);
