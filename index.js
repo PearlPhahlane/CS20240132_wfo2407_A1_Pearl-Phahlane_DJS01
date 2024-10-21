41ce1cf834e803cbf79ae3f1722c65ebda9b8fb4
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-10-21 14:50:43 +0200

    edit given parameters to make them more readable from line 9 to 14

--- index.js
+++ index.js
@@ -6,12 +6,12 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const initialVelocityKmh = 10000; // velocity (km/h)
+const accelerationMs2 = 3; // acceleration (m/s^2)
+const timeSeconds = 3600; // seconds (1 hour)
+const distanceKm = 0; // distance (km)
+const remainingFuelKg = 5000; // remaining fuel (kg)
+const fuelBurnRateKgPerSecond = 0.5; // fuel burn rate (kg/s)
 
 
 const d2 = d + (vel*time) //calcultes new distance
